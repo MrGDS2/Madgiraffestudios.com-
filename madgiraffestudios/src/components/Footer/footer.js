4d80a2d596f85474dbd6033ebde8c3864708b87b
MrGDS2 <mrsonicthunder@gmail.com>
2020-08-02 15:40:15 -0400

    footer fixes

--- madgiraffestudios/src/components/Footer/footer.js
+++ madgiraffestudios/src/components/Footer/footer.js
@@ -6,13 +6,13 @@ class Footer extends Component{
     render(){
         return(
             <div class="footer_bg pt-5 pb-3 bg-light text-center" id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class=" col-md-4 text-center">
+            <div className="container">
+                <div className="row">
+                    <div className=" col-md-4 mb-4">
                         <a href='/'> <img src={giraffeicon} height="50px" alt="logo" id="footer-logo" /></a>
                     </div>
-                    <div class=" col-md-4 text-center">
-                        <div class="row">
+                    <div className=" col-md-4 text-center">
+                        <div className="row">
                             <div className="col-md-3">
 
                                 <p><SocialIcon url="https://www.instagram.com/madgiraffestudios/" bgColor="#8d18b4"/>
@@ -29,7 +29,7 @@ class Footer extends Component{
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4 bg-light"><h6 class="font-weight-bolder line-height">CONTACT INFORMATION</h6><p>Madgiraffestudios@gmail.com</p>
+                    <div className="col-md-4 bg-light"><h6 className="font-weight-bolder line-height">CONTACT INFORMATION</h6><p>Madgiraffestudios@gmail.com</p>
                     </div>
                 </div>
             </div>
