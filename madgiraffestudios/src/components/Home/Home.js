603955943985cae488a77d2bd7dc920da88e4ba1
estherkoko <estherkolade@gmail.com>
2020-08-19 23:40:52 -0400

    fix responsive img, redo landing page

--- madgiraffestudios/src/components/Home/Home.js
+++ madgiraffestudios/src/components/Home/Home.js
@@ -1,50 +1,27 @@
 import React, { useState } from 'react';
-import Painter from '../../Images/landing-img.jpeg'
+import Painter from '../../Images/main-img.png'
+import Spark from '../../Images/spark.png'
 import About from '../About/About';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Form, FormFeedback, Label, FormText, Input, FormGroup, Button } from 'reactstrap';
 
 const Home = () => {
-    const [displayModal, setDisplayModal] = useState(false)
     return (
         <React.Fragment>
 
-            <div className="main-page" id="home">
-                <div className="row mx-5 pt-4 elevator-pitch">
-                    <div className="col-md-10 col-xs-12 mt-5 ">
-                        <strong><p className="emphasized-text">The best way to spark change</p></strong>
-                        <strong><p className="emphasized-text">is to start a conversation.</p></strong>
-                        <h5>I try to capture the in between moments, the moments that most people take for granted.</h5>
+            <div className="main-page mb-5" id="home">
+                <div className="mx-5 pt-4 elevator-pitch">
+                    <div className="text-center container mt-5">
+                        <strong><p className="emphasized-text">The best way to sp
+                            <span><img src={Spark} alt="spark"/></span>rk change</p>
+                        </strong>
+                        <strong><p className="emphasized-text primary-color">is to start a conversation.</p></strong>
+                        <h5 className="pt-4">I try to capture the in between moments, the moments that most people take for granted.</h5>
                         <h5>Stories are not told they are assumed. <span className="oblique-text">~Kae, Creative Owner</span></h5>
-                        <button type="button" className="btn btn-lg shadow-lg booking-btn mt-3 mb-5 p-3" onClick={() => setDisplayModal(true)}>Book Me</button>
-                    </div>
-                    <div className="col-md-2 painter-img mt-5">
-                        <img src={Painter} alt="painter" height="360px" />
+                        <span>
+                            <img src={Painter} alt="painter" className="main-img"/>
+                        </span>
                     </div>
                 </div>
             </div>
-            <Modal isOpen={displayModal} className="mt-5 text-center">
-               <ModalHeader className="bg-primary text-white">Let's Work Together!</ModalHeader>
-                <ModalBody className="font-weight-bold">
-                    <Form>
-                        <FormGroup>
-                            <Label for="receipient name" className="d-block text-left">Fullname</Label>
-                            <Input />
-                            <FormFeedback>You will not be able to see this</FormFeedback>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="email" className="d-block text-left">Email</Label>
-                            <Input />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="subject" className="d-block text-left">Subject</Label>
-                            <Input type="textarea" id="modal-textarea"/>
-                        </FormGroup>
-                        <Button>Send Request</Button>
-                    </Form>
-                </ModalBody>
-                <ModalFooter>
-                </ModalFooter>
-            </Modal>
 
             <About />
         </React.Fragment>
--- madgiraffestudios/src/components/Navigation/NavigationBar.js
+++ madgiraffestudios/src/components/Navigation/NavigationBar.js
@@ -1,29 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Form, FormFeedback, Label, Input, FormGroup, Button } from 'reactstrap';
+
 import giraffeicon from '../../Images/icon.png';
 
-const NavigationBar = () => (
+const NavigationBar = () => {
 
-<div className="pt-4 container" >
-    
+    const [displayModal, setDisplayModal] = useState(false);
 
-    <Navbar expand="lg">
-        <Navbar.Brand href="/home"><img src={giraffeicon} height="50px" width="50px" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto mx-auto nav nav-pills nav-justified" >
-                <Nav.Item><Nav.Link href="/" activeClassName="selected"  className="ml-4 mr-4">Home</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link activeClassName="selected" href="#about" className="ml-4 mr-4">About</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link activeClassName="selected" href="work" className="ml-4 mr-4">Work</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link activeClassName="selected" href="/contact" className="ml-4 mr-4">Contact</Nav.Link></Nav.Item>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-    
-   
+return (
+    <React.Fragment>
+    <div className="pt-4 container" >
+        <Navbar expand="lg">
+            <Navbar.Brand href="/home"><img src={giraffeicon} height="50px" width="50px" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto mx-auto nav nav-pills nav-justified" >
+                    <Nav.Item><Nav.Link href="/" activeClassName="selected"  className="ml-4 mr-4">Home</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link activeClassName="selected" href="#about" className="ml-4 mr-4">About</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link activeClassName="selected" href="work" className="ml-4 mr-4">Work</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link activeClassName="selected" href="/contact" className="ml-4 mr-4">Contact</Nav.Link></Nav.Item>
+                </Nav>
+                <Nav className="ml-auto mx-auto nav nav-pills nav-justified" >
+                    <Nav.Item><button type="button" className="btn rounded shadow-lg booking-btn p-3 float-right" onClick={() => setDisplayModal(true)}>Book Me</button></Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     </div>
-)
+
+    <Modal isOpen={displayModal} className="mt-5 text-center">
+               <ModalHeader className="bg-primary text-white">Let's Work Together!</ModalHeader>
+                <ModalBody className="font-weight-bold">
+                    <Form>
+                        <FormGroup>
+                            <Label for="receipient name" className="d-block text-left">Fullname</Label>
+                            <Input />
+                            <FormFeedback>You will not be able to see this</FormFeedback>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="email" className="d-block text-left">Email</Label>
+                            <Input />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="subject" className="d-block text-left">Subject</Label>
+                            <Input type="textarea" id="modal-textarea"/>
+                        </FormGroup>
+                        <Button onClick={() => setDisplayModal(false)}>Send Request</Button>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                </ModalFooter>
+            </Modal>
+    </React.Fragment>
+    )
+}
 
 
 export default NavigationBar;
