606ab81bffd075359afb32b2128ec16be276a24c
MrGDS2 <mrsonicthunder@gmail.com>
2021-01-04 15:16:53 -0500

    CHANGED: giraffe icon leads to home page

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -22,7 +22,7 @@ return (
             <Navbar.Brand href="/"><img src={giraffeicon} height="50px" width="50px" alt="nav-icon"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto mx-auto nav " >
+                <Nav className="ml-auto mx-auto nav" >
                     <Nav.Item><Nav.Link href="/" activeclassname="selected"  className="ml-4 mr-4">Home</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link activeclassname="selected" href="#about" className="ml-4 mr-4">About</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link activeclassname="selected" href="#gallery" className="ml-4 mr-4">Gallery</Nav.Link></Nav.Item>
