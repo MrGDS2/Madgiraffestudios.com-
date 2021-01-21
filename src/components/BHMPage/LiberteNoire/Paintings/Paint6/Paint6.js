48886ee224c2c79766101dbe0df0935aff74573a
MrGDS2 <mrsonicthunder@gmail.com>
2021-01-21 15:25:11 -0500

    style page fix

--- src/components/BHMPage/LiberteNoire/Paintings/Paint6/Paint6.js
+++ src/components/BHMPage/LiberteNoire/Paintings/Paint6/Paint6.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import firebase from '../../../../../Firebase';
+import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import '../../../LiberteNoire/Paintings/Paintings.scss';
 
@@ -9,44 +10,44 @@ const Paint6 = () => {
 
   const [name, setName] = useState('');
   const [image, setImage] = useState('');
-  const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
 
   firebase.database().ref("Neice").once("value", snapshot => {
     setName(snapshot.child("name").val());
     setImage(snapshot.child("image").val());
-    setPrice(snapshot.child("price").val())
     setDescription(snapshot.child("description").val());
   });
 
 
   return (
     <React.Fragment>
-      <div className="container ">
-        <div className="container pb-5" id="about">
-          <div className="text-center pt-5 mx-auto">
-            <h1 className="showTitle font-weight-bold p-3 secondary-color">
-              Liberté Noir Showcase
+      <Carousel showThumbs={false} showArrows={false} showIndicators={false} showStatus={false}>
+        <div className="container ">
+          <div className="container pb-5" id="about">
+            <div className="text-center pt-5 mx-auto">
+              <h1 className="showTitle font-weight-bold p-3 secondary-color">
+                Liberté Noir Showcase
                             </h1>
+            </div>
           </div>
+          <div className="row mt-5 pb-5">
+            <div className="col-5">
+              <h1 className="align-me font-weight-bold p-3 secondary-color" id="noir-title">
+                {name}
+              </h1>
+            </div>
+            <div className="col-4" >
+              <img src={image} id="noir-img" alt="main-img" />
+            </div>
+            <div className="col-3">
+              <p className="show-text mb-5 studio-text">
+                {description}
+              </p>
+            </div>
+          </div>
+
         </div>
-        <div className="row mt-5 pb-5">
-          <div className="col-5">
-            <h1 className="align-me font-weight-bold p-3 secondary-color" id="noir-title" >
-              {name}
-            </h1>
-          </div>
-          <div className="col-4" >
-            <img src={image} className="pic" id="noir-img" alt="main-img" />
-          </div>
-          <div className="col-3">
-            <p className="show-text mb-5 studio-text">
-              {description}
-            </p>
-          </div>
-        </div>
-        <p className="legend">{price}</p>
-      </div>
+      </Carousel>
     </React.Fragment>
   )
 
