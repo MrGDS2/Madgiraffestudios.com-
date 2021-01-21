2d164a79ac7405d846ab0c808c3c69ab167c4d3c
MrGDS2 <mrsonicthunder@gmail.com>
2021-01-21 00:25:59 -0500

    disable

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -7,42 +7,42 @@ import Booking from '../Booking/Booking';
 
 
 const NavigationBar = () => {
-   
-    const ref= useRef(null);
 
-    const handleClick=()=>{
-     ref.current.showModal();
+    const ref = useRef(null);
+
+    const handleClick = () => {
+        ref.current.showModal();
     }
 
-return (
-    <React.Fragment>
+    return (
+        <React.Fragment>
 
-    <div className="pt-4 container sticky-top nav-top">
-        <Navbar expand="lg">
-            <Navbar.Brand href="/"><img src={giraffeicon} height="50px" width="50px" alt="nav-icon"/></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto mx-auto nav" >
-                    <Nav.Item><Nav.Link href="/" activeclassname="selected"  className="ml-4 mr-4">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link activeclassname="selected" href="/#about" className="ml-4 mr-4">About</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link activeclassname="selected" href="/#gallery" className="ml-4 mr-4">Gallery</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link activeclassname="selected" href="/#footer" className="ml-4 mr-4 mb-2">Contact</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link activeclassname="selected" href="/BHM" className="ml-4 mr-4 mb-2" disabled>Events</Nav.Link></Nav.Item>
-                </Nav>
-                <Nav className="ml-auto mx-auto nav nav-pills nav-justified" > 
-                    <Nav.Item>
-                         <Booking ref={ref}/> 
-                <button type="button" className="btn shadow-lg booking-btn p-3 float-right" onClick={() => handleClick()}>
-                     Book Me</button>
-                </Nav.Item> 
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
+            <div className="pt-4 container sticky-top nav-top">
+                <Navbar expand="lg">
+                    <Navbar.Brand href="/"><img src={giraffeicon} height="50px" width="50px" alt="nav-icon" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto mx-auto nav" >
+                            <Nav.Item><Nav.Link href="/" activeclassname="selected" className="ml-4 mr-4">Home</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link activeclassname="selected" href="/#about" className="ml-4 mr-4">About</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link activeclassname="selected" href="/#gallery" className="ml-4 mr-4">Gallery</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link activeclassname="selected" href="/#footer" className="ml-4 mr-4 mb-2">Contact</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link activeclassname="selected" href="/BHM" className="ml-4 mr-4 mb-2" disabled>Events</Nav.Link></Nav.Item>
+                        </Nav>
+                        <Nav className="ml-auto mx-auto nav nav-pills nav-justified" >
+                            <Nav.Item>
+                                <Booking ref={ref} />
+                                <button type="button" className="btn shadow-lg booking-btn p-3 float-right" onClick={() => handleClick()}>
+                                    Book Me</button>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
 
 
-    
-    </React.Fragment>
+
+        </React.Fragment>
     )
 }
 
