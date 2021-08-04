3acbac777aa2c5817f5a5950e56275140163896f
MrGDS2 <mrsonicthunder@gmail.com>
2021-08-04 19:56:07 -0400

    update to gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -10,50 +10,51 @@ const Gallery = () => {
 
   const gallery = [
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0176.jpg?alt=media&token=ef2452b1-85da-40b0-a527-bb9861bd4a2b' ,
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0176.jpg?alt=media&token=ef2452b1-85da-40b0-a527-bb9861bd4a2b',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FtouchHands.jpg?alt=media&token=8b61b335-f8f1-4b12-ad52-80a629a36fe3' ,
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FtouchHands.jpg?alt=media&token=8b61b335-f8f1-4b12-ad52-80a629a36fe3',
       startIndex: 0,
       description: 'We have to remember to be there for each other in these tough times'
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0084.jpg?alt=media&token=cc154265-98d4-4b85-8ab7-a607f5cd304f',
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0084.jpg?alt=media&token=cc154265-98d4-4b85-8ab7-a607f5cd304f',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FneckGrab.jpg?alt=media&token=9b8146a4-63b1-4b68-9bc7-c01aff9c4cf9',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FneckGrab.jpg?alt=media&token=9b8146a4-63b1-4b68-9bc7-c01aff9c4cf9',
       startIndex: 1,
       description: 'We are survivors, healers, lovers, a soul, a friend'
       
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0156.jpg?alt=media&token=7ff65436-ad49-4379-b0d8-a777708b70b1',
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0156.jpg?alt=media&token=7ff65436-ad49-4379-b0d8-a777708b70b1',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fyou_are_enough.jpg?alt=media&token=da8c06dc-66fa-43b0-a638-2312979c80b8',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fyou_are_enough.jpg?alt=media&token=da8c06dc-66fa-43b0-a638-2312979c80b8',
       startIndex: 2,
       description: 'The way the world is now and black culture is everywhere, it’s really nice to always be in the conversation'
       
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0064.jpg?alt=media&token=61994a40-6007-4737-acdf-058cb814a778',
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fcouple_small.png?alt=media&token=27dd3f0c-77d0-4a78-87dd-ea14e1c97d65',
+      
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fhoney-0001.png?alt=media&token=2400a651-80d1-4de7-99bb-f54995c12e5c',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fhoney-0001.png?alt=media&token=2400a651-80d1-4de7-99bb-f54995c12e5c',
       startIndex: 3,
       description: 'It’s just accepting them for their whole self, their perfections and imperfections'
       
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0215.jpg?alt=media&token=ffd46de6-8cad-4882-840f-db3d6448a147',
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2F_DSC0215.jpg?alt=media&token=ffd46de6-8cad-4882-840f-db3d6448a147',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fhair.jpg?alt=media&token=67ed1c59-dd16-41ce-a243-e77faf1f6ca6',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fhair.jpg?alt=media&token=67ed1c59-dd16-41ce-a243-e77faf1f6ca6',
       startIndex: 4,
       description: 'My Blackness motivates me to rise above that societal illness'
       
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2FIMG_7902_small.png?alt=media&token=8d16fc90-1ab5-4e3b-a0a2-8852edf76c74',
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FPhotoshoots%2Fsmall%2FIMG_7902_small.png?alt=media&token=8d16fc90-1ab5-4e3b-a0a2-8852edf76c74',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FtouchForhead.jpg?alt=media&token=3a6bf83b-97d1-4514-a22b-d63d73ab48a0',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FtouchForhead.jpg?alt=media&token=3a6bf83b-97d1-4514-a22b-d63d73ab48a0',
       startIndex: 5,
       description: 'We have a will to carry on...to be better and do more because you’re born with a handicap'
       
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fembosed-logo-mockup.jpg?alt=media&token=4c505361-c786-42ce-bd97-b7e8d9fb89f6',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Ftongue.jpg?alt=media&token=4f568566-808a-44bb-b4a9-576d851e64e5',
       
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fembosed-logo-mockup.jpg?alt=media&token=4c505361-c786-42ce-bd97-b7e8d9fb89f6',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Ftongue.jpg?alt=media&token=4f568566-808a-44bb-b4a9-576d851e64e5',
       startIndex: 6,
       description: `Custom  company logo on cardstock`
       
@@ -61,17 +62,17 @@ const Gallery = () => {
 	
 
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FLabel-Brand-Mockup-vol7_small.jpg?alt=media&token=27fa9375-b9aa-47bd-8ca8-3c3c84edb06f',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fbodpositivity3.jpg?alt=media&token=156099f7-c726-4e42-90e7-028fbcb1f07c',
       
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FLabel-Brand-Mockup-vol7_small.jpg?alt=media&token=27fa9375-b9aa-47bd-8ca8-3c3c84edb06f',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fbodpositivity3.jpg?alt=media&token=156099f7-c726-4e42-90e7-028fbcb1f07c',
       startIndex: 7,
       description: 'Save the Bees, They make our Honey!'
       
     },
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FmrS.jpg?alt=media&token=bc6e0dab-fc5e-4b0a-aa04-78d580c7cd77',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fblck_wraps.jpg?alt=media&token=c516fa2c-d107-4b30-a682-8c484c416a7c',
       
-      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2FmrS.jpg?alt=media&token=bc6e0dab-fc5e-4b0a-aa04-78d580c7cd77',
+      thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fblck_wraps.jpg?alt=media&token=c516fa2c-d107-4b30-a682-8c484c416a7c',
       startIndex: 8,
       description: 'Gaming icon for streamer on Twitch: Mrgds2'
       
@@ -87,7 +88,7 @@ const Gallery = () => {
     },
 
     {
-      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FIllustrations%2Fsmall%2FBLM_gifs_sm_fst.gif?alt=media&token=7485d541-cd03-4f15-a927-5215507fe39e',
+      original: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FGraphics%2Fbodpositivity3.jpg?alt=media&token=156099f7-c726-4e42-90e7-028fbcb1f07c',
       
       thumbnail: 'https://firebasestorage.googleapis.com/v0/b/madgiraffestudiossite.appspot.com/o/Work%2FIllustrations%2Fdrawing01.jpg?alt=media&token=1b472979-1d6d-4e43-9471-771afbd01443',
       startIndex: 10,
