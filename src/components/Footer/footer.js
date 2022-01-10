c876230f9fb78eb73b50a8634f150091feef23c8
MrGDS2 <mrsonicthunder@gmail.com>
2022-01-10 00:38:03 -0500

    footer 2022 update

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -33,7 +33,7 @@ class Footer extends Component{
                     </div>
                     <div className="row">
                             <div className="col-md-11 mt-4">
-                            <span className="text-center"><p className="copyright text-muted">©2021 by <a href="https://github.com/MrGDS2">MrGDS2</a></p></span>
+                            <span className="text-center"><p className="copyright text-muted">©2022 by <a href="https://github.com/MrGDS2">MrGDS2</a></p></span>
 
                             </div>
                 </div>
